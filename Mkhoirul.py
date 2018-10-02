a26349e626901ececde00eb3e97a6969dbd39490
Muhammad Khoirul Anwar <31978317+Mkhoirul@users.noreply.github.com>
2018-10-02 21:21:16 +0700

    Anyar
    new

--- Mkhoirul.py
+++ Mkhoirul.py
@@ -7,6 +7,14 @@ arr = [n, a, s, r, u, l]
 
 for i in arr:
     print i + "elek"
+    
+array = [2, 6, 4, 7, 11, 9, 22]
+
+for z in array:
+    if z % 2 == 0:
+        array[z] = "#"
+    else:
+        array[z] = "@"
 
 
 
