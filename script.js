1290ff789d49fbba7a3b52400cd71526bee681c6
Sabil <41353160+fesasabil@users.noreply.github.com>
2020-10-21 08:57:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var codeWilayah = 200;
 biayaPanggilan = lamaPanggilan*codeWilayah;
 
 console.log(+biayaPanggilan+ " Rupiah");
+
+var panjang = 20;
+var lebar = 100;
+
+luas = panjang*lebar;
+
+console.log(+luas+ " Meter");
